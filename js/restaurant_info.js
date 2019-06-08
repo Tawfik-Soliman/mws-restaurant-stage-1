2648cdd6fed48cfa70fbd3deb13c2d55b63b06df
Tawfik Soliman <tk.abdullah80@gmail.com>
2019-06-08 17:49:43 +0200

    Adding Mapbox API Key to rendering the map for individual restaurants

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidGF3ZmlrLXNvbGltYW4iLCJhIjoiY2p3bm9sYmcxMDhqOTRhcTlnN3N1N2Q4aiJ9.8q5f4n7VLZGIg3PpP8Ca_Q',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
