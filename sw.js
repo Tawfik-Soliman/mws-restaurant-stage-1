5d5c218ed273c0421c4c951b805a47eebd7f17bf
Tawfik Soliman <tk.abdullah80@gmail.com>
2019-06-14 23:42:27 +0200

    Craeting a cache and caching project files in it

--- sw.js
+++ sw.js
@@ -6,6 +6,37 @@ const cacheName = 'v1';
 self.addEventListener('install', (e) => {
 	console.log('SW installed');
 	//caching files in cache
+	e.waitUntil(
+		caches
+			.open(cacheName)
+			.then(cache => {
+				console.log(cache);
+				cache.addAll([
+					// files to be cached
+					'/',
+					'/index.html',
+					'/restaurant.html',
+					'/js/main.js',
+					'/js/restaurant_info.js',
+					'/js/dbhelper.js',
+					'/css/styles.css',
+					'/data/restaurants.json',
+					'/img/1.jpg',
+					'/img/2.jpg',
+					'/img/3.jpg',
+					'/img/4.jpg',
+					'/img/5.jpg',
+					'/img/6.jpg',
+					'/img/7.jpg',
+					'/img/8.jpg',
+					'/img/9.jpg',
+					'/img/10.jpg'
+					]);
+			})
+			//skip waiting after finishing
+			.then(() => self.skipWaiting())
+
+	);
 });
 
 
